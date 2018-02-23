e2add94117d8091b1be344691820199763ba75f4
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2018-02-24 02:27:55 +0530

    done with skeleton of website

--- local_bookstore/urls.py
+++ local_bookstore/urls.py
@@ -13,9 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from  django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
